00d4da5838c281a6e98792a11e28e3e8128b7519
kevinashley <kashley3141@gmail.com>
2024-08-17 13:36:32 -0700

    shared pageNotFound component

---
+++ pageNotFound.tsx
@@ -0,0 +1,32 @@
+import Grid from "@mui/material/Grid";
+import ReportTwoToneIcon from "@mui/icons-material/ReportTwoTone";
+import Box from "@mui/system/Box";
+import RoutedLink from "@/sharedComponents/routedLink";
+
+export default function PageNotFound() {
+    return (
+        <Grid
+            container
+            maxWidth="md"
+            spacing={1}
+            alignItems="center"
+            sx={{ margin: "auto", justifyContent: "center", height: "100%" }}
+        >
+            <Box
+                sx={{
+                    textAlign: "center",
+                }}
+            >
+                <ReportTwoToneIcon
+                    color={"error"}
+                    sx={{
+                        fontSize: "200px",
+                    }}
+                />
+                <h2>404: Page Not Found</h2>
+                <p>Could not find requested resource</p>
+                <RoutedLink href="/">Return Home</RoutedLink>
+            </Box>
+        </Grid>
+    );
+}
