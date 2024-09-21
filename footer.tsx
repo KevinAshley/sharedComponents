425ac5b966e46c5e97e837d7aab7460f8ad290dd
kevinashley <kashley3141@gmail.com>
2024-09-21 16:02:42 -0700

    footer styling

--- footer.tsx
+++ footer.tsx
@@ -1,37 +1,21 @@
 import Box from "@mui/material/Box";
 import Copyright from "@/sharedComponents/copyright";
 
-const height = "36px;";
-
 const Footer = () => {
     return (
         <Box
             sx={{
-                // height,
-                marginTop: "3rem",
+                marginTop: "4rem",
             }}
         >
             <Box
-                component="footer"
                 sx={{
-                    height,
-                    // marginTop: "3rem",
+                    px: 2,
+                    py: 1,
+                    bgcolor: "var(--mui-palette-dark)",
                 }}
             >
-                <Box
-                    sx={{
-                        px: 2,
-                        py: 1,
-                        bgcolor: "var(--mui-palette-dark)",
-                        position: "fixed",
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        height,
-                    }}
-                >
-                    <Copyright />
-                </Box>
+                <Copyright />
             </Box>
         </Box>
     );
