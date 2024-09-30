95c2f2f254f94c3b8829ea1a111a6b88b46e2be0
kevinashley <kashley3141@gmail.com>
2024-09-29 18:01:04 -0700

    added widths to wrapper components

--- pageWrapper.tsx
+++ pageWrapper.tsx
@@ -5,8 +5,15 @@ import Footer from "@/sharedComponents/footer";
 
 export default function PageWrapper({ children }: { children: ReactNode }) {
     return (
-        <Box sx={{ display: "flex", minHeight: "100vh" }}>
-            <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        <Box sx={{ display: "flex", minHeight: "100vh", width: "100%" }}>
+            <Box
+                sx={{
+                    flex: 1,
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
+                }}
+            >
                 <Header />
                 <Box
                     component="main"
