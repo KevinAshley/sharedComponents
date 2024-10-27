ba75f4d7443216f327f95b7de927351a741e812d
kevinashley <kashley3141@gmail.com>
2024-10-26 20:32:37 -0700

    server-side cloudflare turnstile verification

--- lib/actions/contact.ts
+++ lib/actions/contact.ts
@@ -4,10 +4,25 @@ import { FormValuesIf } from "@/sharedComponents/formComponents/formInterfaces";
 import { PrismaClient, ContactFormSubmission } from "@prisma/client";
 
 const prisma = new PrismaClient();
+const cloudflareTurnstileSecretKey = process.env
+    .CLOUDFLARE_TURNSTILE_SECRET_KEY as string;
 
 export async function addContactFormSubmission(data: FormValuesIf) {
     try {
+        const { turnstileToken } = data;
         const { name, email, message } = data as ContactFormSubmission;
+        let verificationFormData = new FormData();
+        verificationFormData.append("secret", cloudflareTurnstileSecretKey);
+        verificationFormData.append("response", turnstileToken as string);
+        const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+        const turnstileVerifyResponse = await fetch(url, {
+            body: verificationFormData,
+            method: "POST",
+        });
+        const turnstileVerifyOutcome = await turnstileVerifyResponse.json();
+        if (!turnstileVerifyOutcome.success) {
+            throw new Error("Invalid Cloudflare Verification");
+        }
         await prisma.contactFormSubmission.create({
             data: {
                 name,
